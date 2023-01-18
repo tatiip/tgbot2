af2749a4a228b6f22687e103418d7e957275cd29
tatiip <tancia07@yandex.ru>
2023-01-18 16:59:38 +0400

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-            <Button>Закрыть</Button>
+            <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
                 {user?.username}
             </span>
