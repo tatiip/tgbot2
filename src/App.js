690c862c1ef7f3b83cd5e985d1d457f406a36d77
tatiip <tancia07@yandex.ru>
2023-01-18 18:14:07 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import {useEffect} from "react";
-import {useTelegram} from "./components/hooks/useTelegram";
+import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 const tg = window.Telegram.WebApp;
 
@@ -17,7 +19,11 @@ function App() {
   return (
     <div className="App">
         <Header />
-      <button onClick={onToggleButton}>toggle</button>
+        <Routes>
+            <Route index element={<ProductList />}/>
+            <Route path={'form'} element={<Form />}/>
+        </Routes>
+
     </div>
   );
 }
